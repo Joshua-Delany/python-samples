1dbbc0d75993b73a1d8b4c39a505af8c038558cc
Joshua-Delany <jdelany@google.com>
2022-01-12 20:28:54 +0000

    Corrected print statements to use python 3 formatting

--- drive/snippets/change_snippets.py
+++ drive/snippets/change_snippets.py
@@ -23,8 +23,8 @@ import google.auth
 def fetch_start_page_token(drive_service):
     try:
         response = drive_service.changes().getStartPageToken().execute()
-        print
-        'Start token: %s' % response.get('startPageToken')
+        print('Start token: {token}'.format(
+            token=response.get('startPageToken')))
         return response.get('startPageToken')
     except HttpError as err:
         # TODO(developer) - handle error appropriately
@@ -44,8 +44,8 @@ def fetch_changes(drive_service, saved_start_page_token):
                                                     spaces='drive').execute()
             for change in response.get('changes'):
                 # Process change
-                print
-                'Change found for file: %s' % change.get('fileId')
+                print('Change found for file: {file_id}'.format(
+                    file_id=change.get('fileId')))
             if 'newStartPageToken' in response:
                 # Last page, save this token for the next polling interval
                 saved_start_page_token = response.get('newStartPageToken')
