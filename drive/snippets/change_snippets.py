f3b9042501e994d6bf83b929b954a877bddafda1
Joshua-Delany <jdelany@google.com>
2022-01-12 20:23:11 +0000

    Added main function and corrected imports

--- drive/snippets/change_snippets.py
+++ drive/snippets/change_snippets.py
@@ -12,10 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import simplejson
-import io
-from datetime import datetime
-from apiclient import errors
+from __future__ import print_function
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import google.auth
 
 
 def fetch_start_page_token(drive_service):
@@ -45,3 +46,38 @@ def fetch_changes(drive_service, saved_start_page_token):
         page_token = response.get('nextPageToken')
     # [END fetchChanges]
     return saved_start_page_token
+
+
+def main():
+    """Demonstrates usage of fetch_start_page_token and fetch_changes
+    """
+    # Load pre-authorized user credentials from the environment.
+    # TODO(developer) - See https://developers.google.com/identity for
+    # guides on implementing OAuth2 for your application.
+    creds, _ = google.auth.default()
+
+    try:
+        # Create the drive v3 API client
+        drive_service = build('drive', 'v3', credentials=creds)
+
+        # Fetch a page token
+        start_page_token = fetch_start_page_token(drive_service=drive_service)
+
+        # Build and execute request to create a new folder.
+        file_metadata = {
+            'name': 'Invoices',
+            'mimeType': 'application/vnd.google-apps.folder'
+        }
+        drive_service.files().create(body=file_metadata, fields='id').execute()
+
+        # Check for changes since page token was retrieved
+        fetch_changes(drive_service=drive_service,
+                      saved_start_page_token=start_page_token)
+    except HttpError as err:
+        # TODO(developer) - handle error appropriately
+        print('An error occurred: {error}'.format(error=err))
+        raise
+
+
+if __name__ = '__main__':
+    main()
