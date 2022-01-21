ef7505b39becdd50c0a8f96c7bb90f5d3b77cfdd
Joshua-Delany <jdelany@google.com>
2022-01-21 19:49:54 +0000

    Added default auth, imports, and use build()

--- drive/snippets/download_file.py
+++ drive/snippets/download_file.py
@@ -12,9 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import print_function
 
-def download_file(self):
-    drive_service = self.service
+from googleapiclient.discovery import build
+import google.auth
+
+
+def download_file():
+    # Load pre-authorized user credentials from the environment.
+    # TODO(developer) - See https://developers.google.com/identity for
+    # guides on implementing OAuth2 for your application.
+    creds, _ = google.auth.default()
+
+    drive_service = build('drive', 'v2', credentials=creds)
     # [START downloadFile]
     file_id = '0BwwA4oUTeiV1UVNwOHItT0xfa2M'
     request = drive_service.files().get_media(fileId=file_id)
