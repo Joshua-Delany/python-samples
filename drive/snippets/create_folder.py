cf0ab4a4b9b5bcf68196d0695b6b6203e159915e
Joshua-Delany <jdelany@google.com>
2022-01-21 17:57:37 +0000

    Added default auth, imports, and use build() method

--- drive/snippets/create_folder.py
+++ drive/snippets/create_folder.py
@@ -12,9 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import print_function
+
+from googleapiclient.discovery import build
+import google.auth
+
 
 def create_folder(self):
-    drive_service = self.service
+    # Load pre-authorized user credentials from the environment.
+    # TODO(developer) - See https://developers.google.com/identity for
+    # guides on implementing OAuth2 for your application.
+    creds, _ = google.auth.default()
+
+    drive_service = build('drive', 'v2', credentials=creds)
     # [START createFolder]
     file_metadata = {
         'title': 'Invoices',
