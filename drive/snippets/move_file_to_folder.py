3aa00fbb3a2a7c3310962fa16bfb9e1967be2185
Joshua-Delany <jdelany@google.com>
2022-01-21 23:13:10 +0000

    Added default auth, imports, and build(). Removed excluded code and tags.

--- drive/snippets/move_file_to_folder.py
+++ drive/snippets/move_file_to_folder.py
@@ -12,16 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import print_function
 
-def move_file_to_folder(self, real_file_id, real_folder_id):
-    drive_service = self.service
+from googleapiclient.discovery import build
+import google.auth
+
+
+def move_file_to_folder():
+    # Load pre-authorized user credentials from the environment.
+    # TODO(developer) - See https://developers.google.com/identity for
+    # guides on implementing OAuth2 for your application.
+    creds, _ = google.auth.default()
+
+    drive_service = build('drive', 'v2', credentials=creds)
     # [START moveFileToFolder]
     file_id = '1sTWaJ_j7PkjzaBWtNc3IzovK5hQf21FbOw9yLeeLPNQ'
     folder_id = '0BwwA4oUTeiV1TGRPeTVjaWRDY1E'
-    # [START_EXCLUDE silent]
-    file_id = real_file_id
-    folder_id = real_folder_id
-    # [END_EXCLUDE]
     # Retrieve the existing parents to remove
     file = drive_service.files().get(fileId=file_id,
                                      fields='parents').execute()
--- drive/snippets/touch_file.py
+++ drive/snippets/touch_file.py
@@ -12,18 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import print_function
 
-def touch_file(self, real_file_id, real_timestamp):
-    drive_service = self.service
+from googleapiclient.discovery import build
+import google.auth
+
+
+def touch_file():
+    # Load pre-authorized user credentials from the environment.
+    # TODO(developer) - See https://developers.google.com/identity for
+    # guides on implementing OAuth2 for your application.
+    creds, _ = google.auth.default()
+
+    drive_service = build('drive', 'v2', credentials=creds)
     # [START touchFile]
     file_id = '1sTWaJ_j7PkjzaBWtNc3IzovK5hQf21FbOw9yLeeLPNQ'
     file_metadata = {
         'modifiedDate': datetime.utcnow().isoformat() + 'Z'
     }
-    # [START_EXCLUDE silent]
-    file_id = real_file_id
-    file_metadata['modifiedDate'] = real_timestamp
-    # [END_EXCLUDE]
     file = drive_service.files().update(fileId=file_id,
                                         body=file_metadata,
                                         setModifiedDate=True,
