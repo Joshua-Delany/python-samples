a19b7433039f35edeb4a8a0c9ca3a78c8a8bd08d
Joshua-Delany <jdelany@google.com>
2022-01-21 19:54:54 +0000

    Added try-except block to catch errors from reqeusts

--- drive/snippets/download_file.py
+++ drive/snippets/download_file.py
@@ -18,6 +18,7 @@ from __future__ import print_function
 import io
 
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaIoBaseDownload
 import google.auth
 
@@ -28,14 +29,19 @@ def download_file():
     # guides on implementing OAuth2 for your application.
     creds, _ = google.auth.default()
 
-    drive_service = build('drive', 'v2', credentials=creds)
-    file_id = '0BwwA4oUTeiV1UVNwOHItT0xfa2M'
-    request = drive_service.files().get_media(fileId=file_id)
-    fh = io.BytesIO()
-    downloader = MediaIoBaseDownload(fh, request)
-    done = False
-    while done is False:
-        status, done = downloader.next_chunk()
-        print('Download {status}%.'.format(status=int(status.progress() * 100)))
-    return fh.getvalue()
+    try:
+        drive_service = build('drive', 'v2', credentials=creds)
+        file_id = '0BwwA4oUTeiV1UVNwOHItT0xfa2M'
+        request = drive_service.files().get_media(fileId=file_id)
+        fh = io.BytesIO()
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print('Download {status}%.'.format(status=int(status.progress() * 100)))
+        return fh.getvalue()
+    except HttpError as err:
+        # TODO(developer) - handle error appropriately
+        print('An error occurred: {error}'.format(error=err))
+        raise
 # [END drive_download_file]
--- drive/snippets/export_pdf.py
+++ drive/snippets/export_pdf.py
@@ -18,6 +18,7 @@ from __future__ import print_function
 import io
 
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaIoBaseDownload
 import google.auth
 
@@ -28,15 +29,20 @@ def export_pdf():
     # guides on implementing OAuth2 for your application.
     creds, _ = google.auth.default()
 
-    drive_service = build('drive', 'v2', credentials=creds)
-    file_id = '1ZdR3L3qP4Bkq8noWLJHSr_iBau0DNT4Kli4SxNc2YEo'
-    request = drive_service.files().export_media(fileId=file_id,
-                                                 mimeType='application/pdf')
-    fh = io.BytesIO()
-    downloader = MediaIoBaseDownload(fh, request)
-    done = False
-    while done is False:
-        status, done = downloader.next_chunk()
-        print('Download {status}%.'.format(status=int(status.progress() * 100)))
-    return fh.getvalue()
+    try:
+        drive_service = build('drive', 'v2', credentials=creds)
+        file_id = '1ZdR3L3qP4Bkq8noWLJHSr_iBau0DNT4Kli4SxNc2YEo'
+        request = drive_service.files().export_media(fileId=file_id,
+                                                     mimeType='application/pdf')
+        fh = io.BytesIO()
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print('Download {status}%.'.format(status=int(status.progress() * 100)))
+        return fh.getvalue()
+    except HttpError as err:
+        # TODO(developer) - handle error appropriately
+        print('An error occurred: {error}'.format(error=err))
+        raise
 # [END drive_export_pdf]
