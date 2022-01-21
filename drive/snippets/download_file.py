385326d05d3a9703105b6df960d36268947ea724
Joshua-Delany <jdelany@google.com>
2022-01-21 19:43:36 +0000

    Changed print statements to use python3 formatting and .format() Removed excluded code

--- drive/snippets/download_file.py
+++ drive/snippets/download_file.py
@@ -13,20 +13,16 @@
 # limitations under the License.
 
 
-def download_file(self, real_file_id):
+def download_file(self):
     drive_service = self.service
     # [START downloadFile]
     file_id = '0BwwA4oUTeiV1UVNwOHItT0xfa2M'
-    # [START_EXCLUDE silent]
-    file_id = real_file_id
-    # [END_EXCLUDE]
     request = drive_service.files().get_media(fileId=file_id)
     fh = io.BytesIO()
     downloader = MediaIoBaseDownload(fh, request)
     done = False
     while done is False:
         status, done = downloader.next_chunk()
-        print
-        "Download %d%%." % int(status.progress() * 100)
+        print('Download {status}%.'.format(status=int(status.progress() * 100)))
     # [END downloadFile]
     return fh.getvalue()
--- drive/snippets/export_pdf.py
+++ drive/snippets/export_pdf.py
@@ -13,13 +13,10 @@
 # limitations under the License.
 
 
-def export_pdf(self, real_file_id):
+def export_pdf(self):
     drive_service = self.service
     # [START exportPdf]
     file_id = '1ZdR3L3qP4Bkq8noWLJHSr_iBau0DNT4Kli4SxNc2YEo'
-    # [START_EXCLUDE silent]
-    file_id = real_file_id
-    # [END_EXCLUDE]
     request = drive_service.files().export_media(fileId=file_id,
                                                  mimeType='application/pdf')
     fh = io.BytesIO()
@@ -27,7 +24,6 @@ def export_pdf(self, real_file_id):
     done = False
     while done is False:
         status, done = downloader.next_chunk()
-        print
-        "Download %d%%." % int(status.progress() * 100)
+        print('Download {status}%.'.format(status=int(status.progress() * 100)))
     # [END exportPdf]
     return fh.getvalue()
