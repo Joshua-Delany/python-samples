dd3d98ad42ebec23b9847d4bebca4dbe88f57eb8
Joshua-Delany <jdelany@google.com>
2022-01-24 21:38:11 +0000

    Change print statements to use python3 formatting and .format()

--- drive/snippets/fetch_changes.py
+++ drive/snippets/fetch_changes.py
@@ -24,8 +24,8 @@ def fetch_changes(self, saved_start_page_token):
                                                 spaces='drive').execute()
         for change in response.get('items'):
             # Process change
-            print
-            'Change found for file: %s' % change.get('fileId')
+            print('Change found for file: {file_id}'.format(
+                file_id=change.get('fileId')))
         if 'newStartPageToken' in response:
             # Last page, save this token for the next polling interval
             saved_start_page_token = response.get('newStartPageToken')
--- drive/snippets/fetch_start_page_token.py
+++ drive/snippets/fetch_start_page_token.py
@@ -17,7 +17,6 @@ def fetch_start_page_token(self):
     drive_service = self.service
     # [START fetchStartPageToken]
     response = drive_service.changes().getStartPageToken().execute()
-    print
-    'Start token: %s' % response.get('startPageToken')
+    print('Start token: {token}'.format(token=response.get('startPageToken')))
     # [END fetchStartPageToken]
     return response.get('startPageToken')
